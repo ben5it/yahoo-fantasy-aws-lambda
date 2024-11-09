9c10eb594e227a37c1bb66c093e867f6344b92d6
huang <shaozuo.huang@siemens.com>
2024-11-09 15:00:02 +0800

    remove events and tests folder

--- lambda/web_server.py
+++ lambda/web_server.py
@@ -4,7 +4,9 @@ import json
 import os
 import boto3
 import time
-
+import io
+import zipfile
+import base64
 
 import utils
 import fantasy_api as fapi
@@ -88,7 +90,7 @@ def lambda_handler(event, context):
         if 'league_id' not in qs or 'week' not in qs :
             logger.error('Missing parameters in query string, requirede parameters: league_id, week')
             return {
-                'statusCode': 501,
+                'statusCode': 400,
                 'body': json.dumps('Missing parameters in query string, requirede parameters: league_id, week')
             }  
     
@@ -118,8 +120,8 @@ def lambda_handler(event, context):
             last_updated = int(resp['Item']['last_updated'])
             now = int(time.time())
             if state == 'COMPLETED':
-                 # still less than 10 minutes after last update, consider it as up to date
-                if now - last_updated < 600: 
+                 # still less than 15 minutes after last update, consider it as up to date
+                if now - last_updated < 900: 
                     return get_result(league_id, week)
 
                 else: # consider it as out of date, need to run analysis again
@@ -138,11 +140,59 @@ def lambda_handler(event, context):
         # no task id found in db, that means this is the first time to run
         else:
             return run_analysis(parms)
+        
+    elif path == '/api/download':
+
+        qs  = event.get('queryStringParameters')
+        if 'league_id' not in qs or 'week' not in qs :
+            logger.error('Missing parameters in query string, requirede parameters: league_id, week')
+            return {
+                'statusCode': 400,
+                'body': json.dumps('Missing parameters in query string, requirede parameters: league_id, week')
+            }  
+    
+        league_id = int(qs['league_id'])
+        week = int (qs['week'])
+        season = utils.get_season()
+
+        bucket_name = os.environ.get("DATA_BUCKET_NAME")
+        folder_name = f"data/{season}/{league_id}/{week}/"
+        
+        # Get the list of files in the specified folder
+        s3 = boto3.client('s3')
+        objects = s3.list_objects_v2(Bucket=bucket_name, Prefix=folder_name)
+        
+        # Create an in-memory ZIP file
+        zip_buffer = io.BytesIO()
+        with zipfile.ZipFile(zip_buffer, 'w', zipfile.ZIP_DEFLATED) as zip_file:
+            for obj in objects.get('Contents', []):
+                file_key = obj['Key']
+                
+                # Filter for only Excel and PNG files
+                if file_key.lower().endswith('.xlsx') or file_key.lower().endswith('.png'):
+                    file_obj = s3.get_object(Bucket=bucket_name, Key=file_key)
+                    file_content = file_obj['Body'].read()
+                    # Add the file to the ZIP
+                    zip_file.writestr(file_key[len(folder_name):], file_content)
+
+        # Return the ZIP file as a response
+        zip_buffer.seek(0)
+        zip_base64 = base64.b64encode(zip_buffer.getvalue()).decode('utf-8')
+
+        return {
+            "statusCode": 200,
+            "headers": {
+                "Content-Type": "application/zip",
+                "Content-Disposition": f"attachment; filename={league_id}_{week}_result.zip"
+            },
+            "body": zip_base64,
+            "isBase64Encoded": True
+        }
 
     # for other routes, we don't have handler yet
     else:
         return {
-            'statusCode': 501,
+            'statusCode': 404,
             'body': json.dumps(f'No handler found for route {path}')
         }  
 
@@ -188,20 +238,58 @@ def remove_session(sessionId):
 
 def get_result(league_id, week):
 
-    base_url = os.environ.get("BASE_URL") + '/data/'
+    base_url = os.environ.get("BASE_URL") + '/'
     season = utils.get_season()
-    result_excel_file_key = f"{season}/{league_id}/{week}/{league_id}_{week}_result.xlsx"
+
+    prefix = f"data/{season}/{league_id}/{week}/"
+
+    # result excel file key
+    result_excel_file_key = prefix + f"{league_id}_{week}_result.xlsx"
+
     # bar chart file path
-    roto_week_bar_file_path = f"{season}/{league_id}/{week}/roto_bar_w{week:02d}.png"
-    roto_total_bar_file_path = f"{season}/{league_id}/{week}/roto_bar_total.png"
+    roto_week_bar_file_path = prefix + f"roto_bar_w{week:02d}.png"
+    roto_total_bar_file_path = prefix + "roto_bar_total.png"
+
+    radar_chart_teams = get_files_with_pattern(prefix, "radar_team_")
+    radar_chart_forcast = get_files_with_pattern(prefix, "radar_forecast_")
 
     data = {
         "result_excel": base_url+ result_excel_file_key,
-        "roto_week_bar": base_url+ roto_week_bar_file_path,
-        "roto_total_bar": base_url + roto_total_bar_file_path
+        "bar_chart_week": base_url+ roto_week_bar_file_path,
+        "bar_chart_total": base_url + roto_total_bar_file_path,
+        "radar_chart_teams": [ base_url + file_key for file_key in radar_chart_teams ],
+        "radar_chart_forcast": [ base_url + file_key for file_key in radar_chart_forcast ]
     }
 
     return {
         'statusCode': 200,
         'body': json.dumps(data, ensure_ascii=False, indent=4)
     }
+
+def get_files_with_pattern(prefix, pattern):
+    """
+    Get the list of files with a specific name pattern in an S3 bucket.
+    
+    Parameters
+    ----------
+    prefix : str
+        The prefix (folder path) to search within the S3 bucket.
+    pattern : str
+        The name pattern to search for.
+    
+    Returns
+    -------
+    list
+        A list of file keys that match the specified pattern.
+    """
+    s3 = boto3.client('s3')
+    bucket_name = os.environ.get("DATA_BUCKET_NAME")
+    objects = s3.list_objects_v2(Bucket=bucket_name, Prefix=prefix)
+    
+    file_keys = []
+    for obj in objects.get('Contents', []):
+        file_key = obj['Key']
+        if pattern in file_key:
+            file_keys.append(file_key)
+    
+    return file_keys
